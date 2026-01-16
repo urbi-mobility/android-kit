cf0965caf1348a3688ac6fa76cd055fa22bb8e8e
m-derakhshan <43886409+m-derakhshan@users.noreply.github.com>
2026-01-16 11:49:54 +0100

    style(Data-Store): update the crypto setup, make it sealed interface and rearrange the domain layer packages

--- data_store/src/main/java/co/urbi/android/kit/data_store/domain/model/CipherSetup.kt
+++
@@ -1,8 +0,0 @@
-package co.urbi.android.kit.data_store.domain.model
-
-data class CipherSetup(
-    val alias: String,
-    val algorithm: String,
-    val blockMode: String,
-    val padding: String,
-): CryptoSetup
