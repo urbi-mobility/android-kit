5f41b5e8318ce8a937f47604f0b582b8758334b4
Domenico <dommy.g1985@gmail.com>
2026-01-19 16:30:14 +0100

    feat: Added configuration for pubblic repository

--- buildSrc/src/main/kotlin/DataStore.kt
+++ buildSrc/src/main/kotlin/DataStore.kt
@@ -2,4 +2,25 @@
 object DataStore {
     const val MODULE = ":data_store"
     const val NAMESPACE = "co.urbi.android.kit.data_store"
+
+    // Maven Central Publishing
+    const val GROUP_ID = "co.urbi.android.kit"
+    const val ARTIFACT_ID = "datastore"
+    const val POM_NAME = "Urbi DataStore"
+    const val POM_DESCRIPTION = "Encrypted DataStore library for Android with secure preferences support"
+    const val POM_URL = "https://github.com/urbi-mobility/android-kit"
+
+    // License
+    const val LICENSE_NAME = "The Apache License, Version 2.0"
+    const val LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+
+    // Developer
+    const val DEVELOPER_ID = "urbi"
+    const val DEVELOPER_NAME = "Urbi Team"
+    const val DEVELOPER_EMAIL = "info@urbi.co"
+
+    // SCM
+    const val SCM_CONNECTION = "scm:git:git://github.com/urbi-mobility/android-kit.git"
+    const val SCM_DEV_CONNECTION = "scm:git:ssh://github.com:urbi-mobility/android-kit.git"
+    const val SCM_URL = "https://github.com/urbi-mobility/android-kit"
 }
